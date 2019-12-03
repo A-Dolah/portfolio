c1f04d7f5f7cdca06b21d9c826e53dd4f7e06121
A-Dolah <a.dolah.dev@gmail.com>
2019-12-03 14:55:16 +0100

    fix typo

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Link } from "gatsby"
 import Head from "../components/Head"
 
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 
 const NotFound = () => {
   return (
