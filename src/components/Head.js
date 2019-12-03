f9dd376f715084632f568d1d335fab546842b26e
A-Dolah <a.dolah.dev@gmail.com>
2019-12-03 21:48:00 +0100

    Add typing to home page and change favicon

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
+import favicon from "../../static/favicon.png"
 
 const Head = ({ title }) => {
   const data = useStaticQuery(graphql`
@@ -12,7 +13,11 @@ const Head = ({ title }) => {
       }
     }
   `)
-  return <Helmet title={`${title} | ${data.site.siteMetadata.title}`} />
+  return (
+    <Helmet title={`${title} | ${data.site.siteMetadata.title}`}>
+      <link rel="icon" type="image/png" href={favicon} sizes="16x16" />
+    </Helmet>
+  )
 }
 
 export default Head
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,89 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { useState } from "react"
 
 import Layout from "../components/Layout"
 import Head from "../components/Head"
 
+import indexStyles from "./index.module.scss"
+import { randomBytes } from "crypto"
+
 const IndexPage = () => {
+  const [visibleLetters, setVisibleLetters] = useState([{ index: 0 }])
+
+  let hiddenLetters = [
+    "H",
+    "i",
+    ",",
+    " ",
+    "I",
+    "'",
+    "m",
+    " ",
+    "A",
+    "d",
+    "a",
+    "m",
+    ".",
+    " ",
+    "W",
+    "e",
+    "l",
+    "c",
+    "o",
+    "m",
+    "e",
+    " ",
+    "t",
+    "o",
+    " ",
+    "m",
+    "y",
+    " ",
+    "s",
+    "i",
+    "t",
+    "e",
+    "!",
+  ]
+
+  const randomNumberInRange = (min, max) => Math.random() * (max - min) + min
+
+  let timer
+
+  const timerFunc = () => {
+    timer = setTimeout(() => {
+      const index = visibleLetters[visibleLetters.length - 1].index
+      const letter = hiddenLetters.slice(index, index + 1).toString()
+      setVisibleLetters([
+        ...visibleLetters,
+        {
+          letter,
+          index: visibleLetters.length,
+          printTime: randomNumberInRange(100, 400),
+        },
+      ])
+      console.log(visibleLetters[visibleLetters.length - 1].printTime)
+    }, visibleLetters[visibleLetters.length - 1].printTime)
+    if (
+      visibleLetters[visibleLetters.length - 1].index === hiddenLetters.length
+    ) {
+      clearInterval(timer)
+    }
+  }
+
+  timerFunc()
+
   return (
     <Layout>
       <Head title="Home" />
-      <h1>Hello</h1>
-      <h2>Welcome to my blog. Have fun!</h2>
-      <p>
-        Need a developer? <Link to="/contact">Contact me</Link>
-      </p>
+      <h1 className={indexStyles.letter}>
+        {visibleLetters.length > 0 &&
+          visibleLetters.map((letter, index) => {
+            if (index > 0) {
+              return <span key={index}>{letter.letter}</span>
+            }
+            return ""
+          })}
+      </h1>
     </Layout>
   )
 }
