b0d9a46f1fc95b835f42e4ca1dd92df9d36fdaa4
A-Dolah <a.dolah.dev@gmail.com>
2019-12-04 10:25:41 +0100

    refactor printfunction and add paragraph with printing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,89 +1,95 @@
 import React, { useState } from "react"
-
 import Layout from "../components/Layout"
 import Head from "../components/Head"
 
 import indexStyles from "./index.module.scss"
-import { randomBytes } from "crypto"
 
 const IndexPage = () => {
-  const [visibleLetters, setVisibleLetters] = useState([{ index: 0 }])
+  const [visibleHeaderLetters, setVisibleHeaderLetters] = useState([
+    {
+      letter: "",
+      index: 0,
+      printTime: 0,
+    },
+  ])
+  const [visibleParagraphLetters, setVisibleParagraphLetters] = useState([
+    {
+      letter: "",
+      index: 0,
+      printTime: 0,
+    },
+  ])
 
-  let hiddenLetters = [
-    "H",
-    "i",
-    ",",
-    " ",
-    "I",
-    "'",
-    "m",
-    " ",
-    "A",
-    "d",
-    "a",
-    "m",
-    ".",
-    " ",
-    "W",
-    "e",
-    "l",
-    "c",
-    "o",
-    "m",
-    "e",
-    " ",
-    "t",
-    "o",
-    " ",
-    "m",
-    "y",
-    " ",
-    "s",
-    "i",
-    "t",
-    "e",
-    "!",
-  ]
+  const headerString = "Hi, I'm Adam. Welcome to my site! I'm glad you're here!"
+  const paragraphString =
+    "I'm a software developer and bootcamp instructor in Stockholm. Please get in touch!"
 
   const randomNumberInRange = (min, max) => Math.random() * (max - min) + min
 
-  let timer
+  const printFunction = (stringToPrint, state, setStateFunction) => {
+    const lettersToPrint = stringToPrint.split("")
+    const indexOfNextLetter = state[state.length - 1].index
+    const letterToPrint = lettersToPrint.slice(
+      indexOfNextLetter,
+      indexOfNextLetter + 1
+    )
+    setStateFunction([
+      ...state,
+      {
+        letter: letterToPrint,
+        index: state.length,
+      },
+    ])
+  }
 
-  const timerFunc = () => {
-    timer = setTimeout(() => {
-      const index = visibleLetters[visibleLetters.length - 1].index
-      const letter = hiddenLetters.slice(index, index + 1).toString()
-      setVisibleLetters([
-        ...visibleLetters,
-        {
-          letter,
-          index: visibleLetters.length,
-          printTime: randomNumberInRange(100, 400),
-        },
-      ])
-      console.log(visibleLetters[visibleLetters.length - 1].printTime)
-    }, visibleLetters[visibleLetters.length - 1].printTime)
+  const timerFunc = (headerString, paragraphString) => {
+    const timer = setTimeout(() => {
+      if (visibleHeaderLetters.length <= headerString.length) {
+        printFunction(
+          headerString,
+          visibleHeaderLetters,
+          setVisibleHeaderLetters
+        )
+      } else if (visibleParagraphLetters.length <= paragraphString.length) {
+        printFunction(
+          paragraphString,
+          visibleParagraphLetters,
+          setVisibleParagraphLetters
+        )
+      }
+    }, randomNumberInRange(10, 130))
+
     if (
-      visibleLetters[visibleLetters.length - 1].index === hiddenLetters.length
+      visibleParagraphLetters[visibleParagraphLetters.length - 1].index ===
+      paragraphString.length
     ) {
       clearInterval(timer)
     }
   }
 
-  timerFunc()
+  timerFunc(headerString, paragraphString)
 
   return (
     <Layout>
       <Head title="Home" />
       <h1 className={indexStyles.letter}>
-        {visibleLetters.length > 0 &&
-          visibleLetters.map((letter, index) => {
-            if (index > 0) {
-              return <span key={index}>{letter.letter}</span>
-            }
-            return ""
-          })}
+        {visibleHeaderLetters.length > 0 &&
+          visibleHeaderLetters.map((letter, index) => (
+            <span key={index}>{letter.letter}</span>
+          ))}
+        {visibleParagraphLetters.length < 2 && (
+          <span className={indexStyles.blinking}> |</span>
+        )}
       </h1>
+      <p>
+        {visibleParagraphLetters.length > 0 &&
+          visibleParagraphLetters.map((letter, index) => (
+            <span key={index}>{letter.letter}</span>
+          ))}
+        {visibleParagraphLetters.length > 1 && (
+          <span className={indexStyles.blinking}> |</span>
+        )}
+      </p>
     </Layout>
   )
 }
