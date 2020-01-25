8d7e63649fec6132fe51eba1992b618573a4acf0
A-Dolah <a.dolah.dev@gmail.com>
2020-01-25 21:32:22 +0100

    -mTry to fix failed build

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React from "react"
 import Layout from "../components/Layout"
 import Head from "../components/Head"
 import ContentHeader from "../components/ContentHeader"
@@ -7,75 +7,63 @@ const ContactPage = () => {
   const title = "If you have any questions whatsoever"
   const paragraph = "Contact me!"
 
-  const [name, setName] = useState("")
-  const [email, setEmail] = useState("")
-  const [message, setMessage] = useState("")
+  // const [name, setName] = useState("")
+  // const [email, setEmail] = useState("")
+  // const [message, setMessage] = useState("")
 
-  const resetState = () => {
-    setName("")
-    setEmail("")
-    setMessage("")
-  }
+  // const resetState = () => {
+  //   setName("")
+  //   setEmail("")
+  //   setMessage("")
+  // }
 
-  const formData = new FormData()
+  // const formData = new FormData()
 
-  const setFormData = () => {
-    formData.append("name", name)
-    formData.append("email", email)
-    formData.append("message", message)
-  }
+  // const setFormData = () => {
+  //   formData.append("name", name)
+  //   formData.append("email", email)
+  //   formData.append("message", message)
+  // }
 
-  const handleSubmit = e => {
-    e.preventDefault()
-    setFormData()
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: { ...formData },
-    })
-      .then(() => {
-        resetState()
-        alert("Success!")
-      })
-      .catch(error => alert(error))
-  }
+  // const handleSubmit = e => {
+  //   e.preventDefault()
+  //   setFormData()
+  //   fetch("/", {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
+  //     body: { ...formData },
+  //   })
+  //     .then(() => {
+  //       resetState()
+  //       alert("Success!")
+  //     })
+  //     .catch(error => alert(error))
+  // }
 
   return (
     <Layout>
       <Head title="Contact" />
       <ContentHeader title={title} paragraph={paragraph} />
-
-      <form onSubmit={handleSubmit}>
+      <form
+        name="contact"
+        method="post"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+      >
+        <input type="hidden" name="form-name" value="contact" />
         <p>
           <label>
-            Your Name:{" "}
-            <input
-              type="text"
-              name="name"
-              value={name}
-              onChange={e => setName(e.target.value)}
-            />
+            Your Name: <input type="text" name="name" />
           </label>
         </p>
         <p>
           <label>
-            Your Email:{" "}
-            <input
-              type="email"
-              name="email"
-              value={email}
-              onChange={e => setEmail(e.target.value)}
-            />
+            Your Email: <input type="email" name="email" />
           </label>
         </p>
         <p>
           <label>
-            Message:{" "}
-            <textarea
-              name="message"
-              value={message}
-              onChange={e => setMessage(e.target.value)}
-            />
+            Message: <textarea name="message"></textarea>
           </label>
         </p>
         <p>
