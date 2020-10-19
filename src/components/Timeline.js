46a8d2684d1d02eb020c7a8cca2ff67deebb553c
A-Dolah <a.dolah.dev@gmail.com>
2020-10-19 16:14:49 +0200

    small changes to timeline

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -78,6 +78,13 @@ const Timeline = () => {
           }
         }
       }
+      saltpic: file(relativePath: { eq: "images/saltpic.jpg" }) {
+        childImageSharp {
+          fixed(width: 200, quality: 100) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
     }
   `)
 
@@ -89,9 +96,8 @@ const Timeline = () => {
         pictureDimensionExtension={pictureDimensionExtension}
         data={data}
         year="2010"
-        text="I graduated the Science studies and Physical education programme
-              at Polhemskolan (upper secondary school) in Lund. At the time I
-              player a lot of football and saxophone."
+        text="This year I graduated the Science studies and Physical education programme
+              at Polhemskolan (upper secondary school) in Lund."
         cardNumber="one"
         image="student"
         alt="picture of high school graduation"
@@ -102,7 +108,7 @@ const Timeline = () => {
         pictureDimensionExtension={pictureDimensionExtension}
         data={data}
         year="2011"
-        text="This was the year when I started my Bachelor of Laws at Uppsala
+        text="Shortly after graduating upper secondary school I started my Bachelor of Laws at Uppsala
               University. Here is me, my sister and my dad in the moving truck
               on our way to Uppsala."
         cardNumber="two"
@@ -115,8 +121,8 @@ const Timeline = () => {
         pictureDimensionExtension={pictureDimensionExtension}
         data={data}
         year="2014"
-        text="After finishing my Bachelor I continued with my Master of Laws at
-              Uppsala University. During this time I met my lovely girlfriend
+        text="After finishing my Bachelor I continued with my Masters of Laws at
+              Uppsala University. This was also the year during which I met my lovely girlfriend
               Annika."
         cardNumber="three"
         image="adamannika"
@@ -128,9 +134,8 @@ const Timeline = () => {
         pictureDimensionExtension={pictureDimensionExtension}
         data={data}
         year="2015"
-        text="I graduated from Uppsala University in June. The same summer I
-              started to work for the biggest law firm in the Nordics -
-              Mannheimer Swartling."
+        text="I graduated from Uppsala University in June 2015. The same summer I
+              started to work for Mannheimer Swartling which is the biggest law firm in the Nordics."
         cardNumber="four"
         image="graduation"
         alt="picture of graduation in Uppsala"
@@ -141,10 +146,7 @@ const Timeline = () => {
         pictureDimensionExtension={pictureDimensionExtension}
         data={data}
         year="2016-2017"
-        text="During this time I did my clerkship at Lund District Court. I
-              learnt a lot during this time, not only from a legal perspective
-              but also from meeting so many different kinds of people from all
-              over society."
+        text="During this period I did my clerkship at Lund District Court. This was an important learning experience, not only from a legal perspective but also through meeting so many different kinds of people from all over society."
         cardNumber="five"
         image="lundsTR"
         alt="picture of Lund District Court"
@@ -155,7 +157,7 @@ const Timeline = () => {
         pictureDimensionExtension={pictureDimensionExtension}
         data={data}
         year="2018"
-        text="   After finishing my clerkship, I returned to Mannheimer Swartling.
+        text="After finishing my clerkship, I returned to Mannheimer Swartling.
               This was also the year I wrote my first line of code. I even have
               an Evernote note from 2018-09-11 with the heading 'First day of
               learning to code'"
@@ -169,10 +171,10 @@ const Timeline = () => {
         pictureDimensionExtension={pictureDimensionExtension}
         data={data}
         year="2019"
-        text="In 2019, I started to work for the tech-focused law firm Synch.
+        text="This year I started to work for the tech-focused law firm Synch.
               This was also the year when I decided to follow my passion and
               pursue a career in programming. I applied to School of Applied Technology
-              and all of a sudden everything changed!"
+              and all of a sudden I was on a new career path!"
         cardNumber="eight"
         image="linkedin"
         alt="picture of Linkedin post"
@@ -187,6 +189,8 @@ const Timeline = () => {
               offered to stay as an instructor. This was an opportunity too good
               to turn down, so now you find me at Epicenter, where we're teaching the next batch of fantastic developers!"
         cardNumber="nine"
+        image="saltpic"
+        alt="picture of Adam at Salt giving lecture"
       />
     </section>
   )
